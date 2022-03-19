0f92dce7f90a1326a442c4c2f989076cc2b350b3
Lidia BanosPerez <banospel@flip2.engr.oregonstate.edu>
2022-03-19 15:27:16 -0700

    Refactoring

--- travel_buddy/public/js/startMenu.js
+++ travel_buddy/public/js/startMenu.js
@@ -2,15 +2,14 @@
 function getQueryString() {
   var result = {}, queryString = location.search.slice(1),
       re = /([^&=]+)=([^&]*)/g, m;
-
   while (m = re.exec(queryString)) {
     result[decodeURIComponent(m[1])] = decodeURIComponent(m[2].replace(/\+/g, ' '));
-  }
-  return result;
+  } return result;
 }
+var destination = getQueryString()["destination"];
 
 // Set default sentence if destination is undefined
-if (typeof destination === "undefined" || null){
+if (typeof destination === "undefined" || null) {
   document.getElementById("destinationName").innerHTML = "a new place";
   var destination = "travel";
 } else{
@@ -29,7 +28,6 @@ xhr.onload = function(data, status) {
   const response = JSON.parse(xhr.response);
   console.log(response);
   if (xhr.status === 200) {
-
     // Set image paths
     activityImage.src = response.imagePath0;
     restaurantImage.src = response.imagePath1;
